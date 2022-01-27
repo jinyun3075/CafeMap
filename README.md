5025c4734fa056518e9e6bb12bc7968473a0ccd5
jinyunjae <jinyun3075@naver.com>
2022-01-27 23:25:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# CafeMap
+## 사용환경
+- vscode: 1.63.2
+- node: 16.13.1
+- MongoDB: 5.0.5
+- 카카오 맵 api
+## 실행 방법
+- git clone ~~ . 깃을 받아옵니다.
+- 클론 폴더로 이동
+- 터미널에 'npm instal bcrypt express cors dotenv mongoose multer body-parser path fs' 입력
+- view -> index.html 78번 줄 appkey= 뒤에 카카오 맵 key 입력
+- 터미널 'npm start' 입력해서 서버 실행 후 view 폴더에 index 페이지 실행
+
+## ~ing
--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = async () => {
-    mongoose.connect(process.env.DATA_URL);
-    // mongoose.connect("mongodb://localhost:27017/test");
+    // mongoose.connect(process.env.DATA_URL);
+    mongoose.connect("mongodb://localhost:27017/test");
     const db = mongoose.connection;
     db.on('error', () => {
         console.log("DB연결 실패");
